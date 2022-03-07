c072b14aa928a97c833b90d53478d4834ed66700
gracewest333 <gracewest333@gmail.com>
2022-03-07 12:35:20 +0000

    game works
    successfully merged fatimas and mine code

--- Cyber_Sercurity_Game/Assets/ShipHealth.cs
+++ Cyber_Sercurity_Game/Assets/ShipHealth.cs
@@ -6,15 +6,16 @@ public class ShipHealth : MonoBehaviour
 {
 
     public int maxHealth=100;
-    public int currentHealth;
+    public int currentHealth=2;
     public HealthBar healthBar;
     public GameObject TeamMate;
     public bool RepairedShip=false;
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth=maxHealth;
+        //currentHealth=maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        healthBar.setHealth(currentHealth);
     }
 
     // Update is called once per frame
--- Cyber_Sercurity_Game/Assets/TeamMemberAppear.cs
+++ Cyber_Sercurity_Game/Assets/TeamMemberAppear.cs
@@ -5,26 +5,15 @@ using UnityEngine;
 public class TeamMemberAppear : MonoBehaviour
 {
     public GameObject TeamMember;
+    public bool appeared=false;
+   
 
-    public void Update()
+    void Update()
     {
-        if (Input.GetKeyDown (KeyCode.L)){
+        if (Input.GetKeyDown (KeyCode.L) && appeared == false) {
             TeamMember.SetActive(true);
+            appeared=true;
         }
     }
 
-
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
-
-    //Update is called once per frame
-    //void Update()
-    //{
-        //if (Input.GetKeyDown (KeyCode.L)) {
-            //Instantiate(TeamMember, new Vector3(-4.722f ,-8.774f ,9.642f ), Quaternion.identity);
-        ///}
-    //}
 }
